c108b94b462ae5225f5f2de6943f9dd3074a4334
JaLongin <neznayupochemu1@gmail.com>
2022-12-29 14:46:23 +0300

    feat: implement limiting number of products per cart page

--- online-store/src/pages/Cart/cart.ts
+++ online-store/src/pages/Cart/cart.ts
@@ -3,6 +3,7 @@ import { Iproduct } from "../../components/model/model";
 import { giveEventListenersToButtonsInCart } from "../../components/cartArr/cartArr";
 
 let response: Iproduct[];
+let currentPage: number = 1;
 const API = "https://dummyjson.com/products?limit=100";
 
 const getProducts = async () => {
@@ -22,9 +23,10 @@ export default {
     return `
       <div class="home-content">
         <div class="filter">
-          <input type="number"></number>
+          <input id="prods-per-cart" type="number" min="1" value="0"></number>
           <div class="turn-pages" style="display: flex">
           <button class="btn prev-page">/<</button>
+          <div id="current-page">${currentPage}</div>
           <button class="btn next-page">/></button>
           </div>
        </div>
@@ -36,7 +38,38 @@ export default {
   },
   afterRender: async () => {
     giveEventListenersToButtonsInCart();
+    pageFiltering();
   },
 };
 
 export { response };
+
+function pageFiltering(){
+  const currentPageEl = document.querySelector('#current-page') as HTMLElement;
+const prodsPerCart = document.querySelector('#prods-per-cart') as HTMLInputElement;
+const allCards: NodeListOf<Element> = document.querySelectorAll('.home-card');
+prodsPerCart.addEventListener('change',function(){
+  allCards.forEach((card, index) => {
+    if (Number(prodsPerCart.value) > 0){
+      currentPage = 1;
+      currentPageEl.innerHTML = `${currentPage}`
+      if (!(index >= Number(prodsPerCart.value) * (currentPage - 1)
+      && index < Number(prodsPerCart.value) * currentPage)){
+        if (!card.classList.contains('hidden')){
+          card.classList.add('hidden'); 
+        }
+      }
+      else {
+        if (card.classList.contains('hidden')){
+          card.classList.remove('hidden');
+        }
+      }
+
+      console.log('I tried');
+    }
+    else {
+      console.log(prodsPerCart.value);
+    }
+  })
+})
+}
